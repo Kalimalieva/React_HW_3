33e67b7813f4e2b0176f590b65094d44600ae69e
Kalimalieva <105165874+Kalimalieva@users.noreply.github.com>
2022-07-22 18:51:18 +0600

    hw_3

---
+++ src/pages/portfolioPage/PortfolioPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class PortfolioPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.nameUser.map((user,key)=> <li key={key}>{user}</li>)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default PortfolioPage;
